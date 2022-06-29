06fcee91c3a1c2f2384d4faa1eef199dbfc26d3c
Maxime Herblot <maximeherblot123@gmail.com>
2022-06-29 16:18:19 +0200

    edit

---
+++ build/precache-manifest.9876c64ed75df2cbda63af0b79915f80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "26a58de9bcc44b95af64",
+    "url": "/static/css/main.daaff65e.chunk.css"
+  },
+  {
+    "revision": "26a58de9bcc44b95af64",
+    "url": "/static/js/main.b7e24a17.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  },
+  {
+    "revision": "316cc844a04bf0ef5f9d",
+    "url": "/static/js/2.926424c2.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "ae26ac717af0c0d0377016849bfe743b",
+    "url": "/index.html"
+  }
+];
